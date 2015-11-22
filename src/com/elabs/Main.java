71ee6609b3eab651cbca1dceb76f11d9475d5116
Yvan Pearson <ypearson@nklabs.com>
2015-11-22 18:14:47 -0500

    Correctly using interface to implement a call back

--- src/com/elabs/Main.java
+++ src/com/elabs/Main.java
@@ -4,99 +4,56 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SomeClass1 sc1 = new SomeClass1(5);
-        SomeClass2 sc2 = new SomeClass2(5);
-        InterfaceIntVar[] obj = {sc1,sc2};
-
-        for (InterfaceIntVar o : obj) {
-            o.setSomeVar(10);
-            o.printSomeVar();
-        }
-
-        SomeClass3 sc3 = new SomeClass3(sc1);
-        sc3.doStuff();
     }
 }
 
-class SomeClass3 {
+class SomeClass1 {
 
-    private InterfaceIntVar mIv;
+    InterfaceIntVar mIv;
 
-    public SomeClass3(InterfaceIntVar iv)
-    {
+    public SomeClass1 (InterfaceIntVar iv){
+
         mIv = iv;
+
     }
 
-    public void doStuff() {
+    public void doWork() {
 
         mIv.printSomeVar();
 
     }
-}
 
-
-class SomeClass1 implements InterfaceIntVar{
-
-    private int var;
-
-    public SomeClass1(int v)
-    {
-        var = v;
-    }
-
-    @Override
-    public int getSomeVar() {
-         return var + 1;
-
-    }
-
-    @Override
-    public void someCallback() {
-
-    }
-
-    @Override
-    public void setSomeVar(int v) {
-
-        var += v;
-    }
-
-    @Override
-    public void printSomeVar() {
-
-        System.out.println(Integer.toString(var));
-    }
 }
 
 class SomeClass2 implements InterfaceIntVar{
 
-    private int var;
+    private SomeClass1 sc1;
+
+    public SomeClass2() {
+
+        sc1 = new SomeClass1(this);
+    }
+
+    @Override
+    public int getSomeVar() {
+        return 0;
+    }
+
+    @Override
+    public void setSomeVar(int v) {
+
+    }
+
+    @Override
+    public void printSomeVar() {
+
+    }
 
     @Override
     public void someCallback() {
 
     }
 
-    public SomeClass2(int v)
-    {
-        var = v;
-    }
 
-    @Override
-    public int getSomeVar() {
-        return var + 10;
-
-    }
-
-    @Override
-    public void setSomeVar(int v) {
-
-        var += v+10;
-    }
-
-    @Override
-    public void printSomeVar() {
-
-        System.out.println(Integer.toString(var));
-    }
 }
+
